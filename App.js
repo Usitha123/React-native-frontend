1811d44fc762992741b3bf905e14bd60fc9d84bf
Usitha <103616801+Usitha123@users.noreply.github.com>
2024-11-15 09:19:28 +0530

    create

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Mystack from './Mystack';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      
+      <Mystack />
+    </NavigationContainer>
+  );
+};
+
+export default App;
